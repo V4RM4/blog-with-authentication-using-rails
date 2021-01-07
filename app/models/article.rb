b32a491dbd21766e99331365203c8ac09f8730b2
V4RM4 <vaisakhsvarma@gmail.com>
2021-01-07 16:52:32 +0530

    one-to-many associations added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, :content, presence: true, length: {minimum: 6, maximum: 100}
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_084724) do
+ActiveRecord::Schema.define(version: 2021_01_07_103322) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
